2a64183e043bb9a148f3ecc8aff394a0b63d9fe5
= <baltaz.samuel@gmail.com>
2026-03-09 16:42:26 -0300

    fix: remove redirect logic from API response interceptor

--- tmdb-front/tmdb-front/src/services/api.ts
+++ tmdb-front/tmdb-front/src/services/api.ts
@@ -56,13 +56,7 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response: any) => response,
   (error: any) => {
-    if (error.response?.status === 401) {
-      window.location.href = '/login';
-    }
-    if (error.response?.status === 403) {
-      window.location.href = '/';
-    }
-    
+
     return Promise.reject(error);
   }
 );
